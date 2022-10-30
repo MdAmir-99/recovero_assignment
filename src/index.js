7f923bbabf4e0fe0514284b073fba21ffe8edeaa
https://github.com/MdAmir-99/radon.git <mdamir1712@gmail.com>
2022-10-30 22:11:46 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -30,7 +30,11 @@ app.use('/', routes)
 // Heroku Deploy
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static("client/dist"))
+    // app.use(express.static("client/dist"))
+    const path = require('path');
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+    })
 }
 
 app.listen(PORT , () => {
