3b44eafe8b843e0be4a8f23a2aa81e608a273168
https://github.com/MdAmir-99/radon.git <mdamir1712@gmail.com>
2022-10-30 20:50:50 +0530

    Changes in Backend

--- src/index.js
+++ src/index.js
@@ -31,10 +31,6 @@ app.use('/', routes)
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static("client/dist"))
-    const path = require('path');
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
-    })
 }
 
 app.listen(PORT , () => {
