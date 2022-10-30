f6139a1acae1cd70418fbcf415ac9939c3233254
https://github.com/MdAmir-99/radon.git <mdamir1712@gmail.com>
2022-10-30 21:50:52 +0530

    demo

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <Router>
       <Navbar />
         <Routes>
-          <Route path='/' element={<Login />} />
+          <Route exact path='/' element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<Dashboard />} />
